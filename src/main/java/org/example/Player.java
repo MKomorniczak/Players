15c88756df59fcb2960c6f15090cd3b366157379
retaz <retazz1991@gmail.com>
2021-02-10 10:16:59 +0100

    Initial commit

---
+++ src/main/java/org/example/Player.java
@@ -0,0 +1,90 @@
+package org.example;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class Player {
+    private int id;
+    private String firstName;
+    private String lastName;
+
+    public Player() {
+    }
+
+    public Player(int id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "Player{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Player player = (Player) o;
+        return id == player.id && Objects.equals(firstName, player.firstName) && Objects.equals(lastName, player.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, firstName, lastName);
+    }
+
+    public static void main(String[] args) {
+         List<Player> players = new ArrayList<>();
+         players.add
+
+
+    }
+
+    public List<Player> serialize(List<Player> source, String filtPath){
+        try(
+                FileOutputStream fileOutputStream = new FileOutputStream(filtPath);
+                ) {
+            out.writeObject(source);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+}
